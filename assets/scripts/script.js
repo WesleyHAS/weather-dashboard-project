40053b7553f5660de2cd581e5c5a6ebcc559004b
Wes <wesley.has@outlook.com>
2023-08-21 21:35:46 -0600

    added comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+// API key for OpenWeatherMap
 var apiKey = '98157aedd6d841e98672d290b1fdd56f';
 
+// Element variables
 var fetchButton = document.getElementById('search-button');
 var currentDayWeather = document.getElementById('current-day-weather');
 var temp = document.getElementById('current-temp');
@@ -13,7 +15,7 @@ var messageDisplay = document.getElementById('message-display'); // Define the m
 
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-
+// Clear history button event listener
 var clearHistoryButton = document.getElementById('clear-history-button');
 clearHistoryButton.addEventListener('click', function() {
   clearHistory();
@@ -21,38 +23,49 @@ clearHistoryButton.addEventListener('click', function() {
   location.reload(); // Reload the page
 });
 
+// Function to clear search history
 function clearHistory() {
   localStorage.removeItem("cities");
 }
 
+// Function to fetch weather data from the API
 function getApi(city) {
   var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + '&units=metric';
 
   fetch(queryUrl)
-  .then(function (response) {
-    if (!response.ok) {
-      throw new Error("City not found"); // Throw an error for invalid city
-    }
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('Fetch Response \n-------------');
-    console.log(data);
-    temp.textContent = 'Temp: ' + data.main.temp + '°C';
-    humidity.textContent = 'Humidity: ' + data.main.humidity + '%';
-    wind.textContent = 'Wind: ' + Math.round(data.wind.speed) * 3600/1000 + 'kph';
-    fiveDayForecast(data.coord.lat, data.coord.lon);
-    messageDisplay.textContent = ""; // Clear the message display
-
-    // If the city is valid, save it to history and create a new button
-    saveHistory(city);
-  })
-  .catch(function (error) {
-    console.error('Error fetching data:', error);
-    messageDisplay.textContent = "Invalid city entered. Please try again.";
-  });
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      
+      var currentDate = new Date(); // Get the current date
+      var formattedDate = currentDate.toLocaleDateString('en-US');
+      var currentDateElement = document.getElementById('current-date'); // Get the current date element
+      currentDateElement.textContent = 'Date: ' + formattedDate;
+      
+      // Display current weather information
+      temp.textContent = 'Temp: ' + data.main.temp + '°C';
+      humidity.textContent = 'Humidity: ' + data.main.humidity + '%';
+      wind.textContent = 'Wind: ' + Math.round(data.wind.speed) * 3600/1000 + 'kph';
+      
+      // Fetch and display 5-day forecast
+      fiveDayForecast(data.coord.lat, data.coord.lon);
+      
+      messageDisplay.textContent = ""; // Clear the message display
+      
+      // If the city is valid, save it to history and create a new button
+      saveHistory(city);
+    })
+    .catch(function (error) {
+      console.error('Error fetching data:', error);
+      messageDisplay.textContent = "Invalid city entered. Please try again.";
+    });
 }
 
+// Function to fetch and display 5-day forecast
 function fiveDayForecast(lat, lon) {
   var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
@@ -61,7 +74,6 @@ function fiveDayForecast(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
 
       var forecastList = data.list;
 
@@ -69,7 +81,15 @@ function fiveDayForecast(lat, lon) {
 
       for (var i = 0; i < forecastList.length; i = i + 8) {
         var dayForecastContainer = document.createElement('div'); // Create a container for each day's forecast
-        dayForecastContainer.classList.add('day-forecast'); // You can add a class for styling if needed
+        dayForecastContainer.classList.add('forecast-card', 'day-forecast'); // Add the styles as classes
+
+        var forecastDate = new Date(forecastList[i].dt * 1000); // Convert timestamp to date object
+        var dayOfWeek = forecastDate.toLocaleDateString('en-US', { weekday: 'long' }); // Get the day of the week
+        var date = forecastDate.toLocaleDateString('en-US'); // Get the actual date
+
+        var dayInfo = document.createElement('div'); // Create an element for the day information
+        dayInfo.textContent = dayOfWeek + ', ' + date;
+        dayForecastContainer.appendChild(dayInfo); // Append the day information element
 
         var nextDaysTemp = document.createElement('div');
         var nextDaysHumidity = document.createElement('div');
@@ -92,6 +112,7 @@ function fiveDayForecast(lat, lon) {
     });
 }
 
+// Search button click event listener
 fetchButton.addEventListener('click', function() {
   var city = inputCity.value.trim();
   if (!city) {
@@ -104,6 +125,7 @@ fetchButton.addEventListener('click', function() {
 });
 
 
+// Function to save city search history
 function saveHistory(cityValue) {
   var cityArray = JSON.parse(localStorage.getItem("cities")) || [];
   var historyButtons = document.getElementById('history-buttons');
@@ -130,6 +152,7 @@ function saveHistory(cityValue) {
 }
 
 
+// Event listener for history buttons
 var historyButtons = document.getElementById('history-buttons');
 historyButtons.addEventListener('click', function(event) {
   if (event.target.classList.contains('city-button')) {
